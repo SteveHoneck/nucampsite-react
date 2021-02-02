b722b090e31947494d511f95f1bc39106f0ff4f2
SteveDev <stevehoneck@gmail.com>
2021-02-02 11:38:55 -0500

    Post 'Exercise: Functional Components'

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Card,
   CardImg,
-  CardImgOverlay,
   CardText,
   CardBody,
   CardTitle,
 } from "reactstrap";
 
-class CampsiteInfo extends Component {
-  renderCampsite(campsite) {
+function RenderCampsite({campsite}) { //This funciton is receiving a props object from CampsiteInfo below, we want the "campsite" property of the object, so use curly braces to destructure it.
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+function RenderComments({comments}) {//This funciton is receiving a props object from CampsiteInfo function below, we want the "comments" property of the object, so use curly braces to destructure it.
+  if (comments) { //if argument comments contain something (as opposed to nothing which would evaluate to falsy) enter loop, otherwise return a blank <div>.
     return (
       <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  }
-
-  renderComments(comments) {
-    if (comments) { //if argument comments contain something (as opposed to nothing which would evaluate to falsy) enter loop, otherwise return a blank <div>.
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map((comment) => ( //Argument "comments" is an array from campsites.js. It is made available/passed into this function by the render function/method below.  JSX can only accept one return value from the .map loop so to make the comment.text and comment.author appear on different lines, they must be wrapped in an outer <div> with <p> and line break <br/> to provide proper spacing. Multiple <div>s can be used around the comment.text and comment.date, but they must be in one outer return <div> (spacing is not as nice with this method).
+        <h4>Comments</h4>
+        {comments.map(comment => { //Argument "comments" is an array from campsites.js. It is made available/passed into this function by the render function/method below.  JSX can only accept one return value from the .map loop so to make the comment.text and comment.author appear on different lines, they must be wrapped in an outer <div> with <p> and line break <br/> to provide proper spacing. Multiple <div>s can be used around the comment.text and comment.date, but they must be in one outer return <div> (spacing is not as nice with this method).
+          return ( //I did not have this "return" in here & it was working after week 2 workshop, but the lectures had it in here so I added it.
             <div key={comment.id}>
               <p>{comment.text} <br /> --{comment.author},{" "}
               {new Intl.DateTimeFormat("en-US", {
@@ -37,26 +36,26 @@ class CampsiteInfo extends Component {
                 day: "2-digit",
               }).format(new Date(Date.parse(comment.date)))}</p>
             </div>
-          ))}
-        </div>
-      );
-    }
-    return (<div />);
+          );
+            })}
+      </div>
+    );
   }
+  return <div />;
+}
 
-  render() {
-    if (this.props.campsite) {
-      return (
-        <div className="container">
-          <div className="row">
-            {this.renderCampsite(this.props.campsite)}{" "}
-            {this.renderComments(this.props.campsite.comments)} {/*This is passing the "comments" array from the campsites.js file into the renderComments method/function. "comments" array is being made available as "props" to this CampsiteInfoComponent.js file because the CampsiteInfo component is called with an attribute named "campsite" inside the JSX tag in the DirectoryComponent.js file, which passes it as "props" to this file.*/}
-          </div>
+function CampsiteInfo(props) { //This is recieving a campsite object as props from MainComponent which is a campsite from the campsites.js file that was filtered in MainComponent.js.
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} /> {/*This is passing the "comments" array from the campsites.js file into the RenderComments method/function. "comments" array is being made available as "props" to this CampsiteInfoComponent.js file because the CampsiteInfo component is called with an attribute named "campsite" inside the JSX tag in the MainComponent.js file, which passes it as "props" to this file.*/}
         </div>
-      );
-    }
-    return (<div />);
+      </div>
+    );
   }
+  return <div />;
 }
 
 export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component {
-    render() {
-        const directory = this.props.campsites.map(campsite => { //Map goes through all the campsites from the local state and will make a new array where each array item contains the below set of JSX elements but using a different campsite for each item. Then the whole array is rendered insid the bootstrap row in the return function below. ".props. was .state., however, when the campsites information was removed from this JS file and moved to the App JS file, campsites information is no longer a state property of this file.  It is passed to this file through the "props" command.
-            return (//not the same return from the whole component, only for the arrow function. key= added as the unique key for topmost element as required/recommended by React 
-                <div key={campsite.id} className = "col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}> {/*Recieve the onClick as a prop from MainComponent.js and pass to it the Id of the campsite that was clicked on.  When a card is clicked on, the onCampsiteSelect method in the MainComponent.js will fire using the campsite's Id & it will store the Id of the campsite that was clicked on in the Main components states selectedCampsite property.*/}
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+function RenderDirectoryItem({campsite, onClick}){//Will be responsible for rendering each card with different campsite details. Fuctional components only accept 1 props object as an argument. Props object has been destructured in the argument list here.
+    return (
+        <Card onClick={() => onClick(campsite.id)}> {/*Recieve the onClick as a prop from MainComponent.js and pass to it the Id of the campsite that was clicked on.  When a card is clicked on, the onCampsiteSelect method in the MainComponent.js will fire using the campsite's Id & it will store the Id of the campsite that was clicked on in the Main components states selectedCampsite property.*/}
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+} 
 
-        return( //this is the return that sends data from this component to the parent component.  All other returns are just passing data around within this component.
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+function Directory(props) {
+    const directory = props.campsites.map(campsite => { //Map goes through all the campsites from the local state of the MainComponent.js file and will make a new array where each array item contains the below set of JSX elements but using a different campsite for each item. Then the whole array is rendered insid the bootstrap column in the return function below. Campsites is passed to this file through the "props" command.
+        return (//not the same return from the whole component, only for the arrow function. key= added as the unique key for topmost element as required/recommended by React 
+            <div key={campsite.id} className = "col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} /> {/*Call RenderDirectoryItem functional component and pass it campsite (coming from .map method I think?) and onClick (coming from MainComponent.js file where it is passed to Directory JSX tag).*/}
             </div>
         );
-    }
+    });
+
+    return( //this is the return that sends data from this component to the parent component.  All other returns are just passing data around within this component.
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
