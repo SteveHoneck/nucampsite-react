a5d15d78237e900126c237d609130d7dffc88fc6
SteveDev <stevehoneck@gmail.com>
2021-06-28 00:00:44 -0400

    Updated comment test

--- cypress/integration/comments_spec.js
+++ cypress/integration/comments_spec.js
@@ -8,8 +8,14 @@ describe('Leaving comments', () => {
         cy.get('textarea[name="text"]').type('Cypress test comment: user NOT logged in')
         cy.get('select').select('3').should('have.value', '3')
         cy.get('button[value="submit"]').click()
-        cy.get('[class="col-md-5 m-1"]').children().should('not.contain', 'NOT')//POTENETIAL ERROR: Gets the column the comments are in, need a better selector than the class because it could change or be used somewhere else. A better way to do this is to verify if the browser alert pops up.
+        
+        cy.on('window:alert', (alertText) => { //POTENTIAL ERROR: does not account for the scenario of the alert window not popping up (remove "cy.get('button[value="submit"]').click()" for example problem)
+            expect(alertText).to.contain('Your comment could not be posted')
+        })
+        
+        cy.get('[class="col-md-5 m-1"]').children().should('not.contain', 'NOT')//POTENETIAL ERROR: Gets the column the comments are in, need a better selector than the class because it could change or be used somewhere else.
     })
+
 
     it('checks that an authorized user can post a comment', () => {
         //Below UI login to be replaced with "cy.login()" when working
@@ -29,19 +35,7 @@ describe('Leaving comments', () => {
         cy.get('[class="col-md-5 m-1"]').children().should('contain', 'IS')//POTENETIAL ERROR: Gets the column the comments are in, need a better selector than the class because it could change or be used somewhere else.
         //Test that the comment exists and that the date & author is correct
     })
+    
 
 //Need to erase comments in database after tests
 })
-
-
-
-
-/* 
-Navigate to directory
-navigate to a campsite
-click on comment button
-change rating
-type in comment
-hit submit
-verify it can not be submitted
-*/
